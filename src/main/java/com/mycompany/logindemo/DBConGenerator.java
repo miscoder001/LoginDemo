dc90ec6361a29a0f8f97a0628e27e51c550756d4
miscoder001 <38873656+miscoder001@users.noreply.github.com>
2023-12-20 10:29:37 +0800

    DBConGenerator
    DBConGenerator 加入

---
+++ src/main/java/com/mycompany/logindemo/DBConGenerator.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.logindemo;
+
+import java.sql.*;
+
+/**
+ *
+ * @author student
+ */
+public class DBConGenerator {
+
+    String username="root";
+    String passwd="12345";
+    String cstring = "jdbc:mariadb://127.0.0.1/webdb";
+    
+    public DBConGenerator() {
+        try{
+            Class.forName("org.mariadb.jdbc.Driver");            
+        }catch(Exception e ) {            
+        }
+    }
+    
+    public Connection getConnction() {
+        Connection con;
+        try{
+            con = DriverManager.getConnection(cstring,username,passwd);
+            return con;
+        } catch( Exception e) {
+            return null;
+        }        
+    }
+    
+}
